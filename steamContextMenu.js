c39ab6da171d145034db26f19d3c8553934f6e75
Derek <derek@skylark95.com>
2015-08-07 22:03:47 -0500

    inital commit

---
+++ steamContextMenu.js
@@ -0,0 +1,7 @@
+chrome.contextMenus.create({
+    "title": "Search Steam for '%s'",
+    "contexts": ["selection"],
+    "onclick": function (info) {
+        chrome.tabs.create({url: 'http://store.steampowered.com/search/#term=' + encodeURIComponent(info.selectionText)});
+    }
+});
