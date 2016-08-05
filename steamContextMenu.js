d133598d8c0a734a112d19db87b5e2f51b81c361
Derek <derek@skylark95.com>
2016-08-04 21:27:21 -0500

    add menu for steamdb

--- steamContextMenu.js
+++ steamContextMenu.js
@@ -1,7 +1,21 @@
+/*
+ * Search Steam Context Menu
+ */
 chrome.contextMenus.create({
     "title": "Search Steam for '%s'",
     "contexts": ["selection"],
     "onclick": function (info) {
         chrome.tabs.create({url: 'http://store.steampowered.com/search/?term=' + encodeURIComponent(info.selectionText)});
+    }
+});
+
+/*
+ * Search Steam DB Context Menu
+ */
+chrome.contextMenus.create({
+    "title": "Search Steam DB for '%s'",
+    "contexts": ["selection"],
+    "onclick": function (info) {
+        chrome.tabs.create({url: 'https://steamdb.info/search/?q=' + encodeURIComponent(info.selectionText)});
     }
 });
