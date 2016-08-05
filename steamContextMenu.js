1092cf451da813a92a96d16f3e254b36e23df489
Derek <derek@skylark95.com>
2016-08-04 23:05:44 -0500

    updates to use new options

--- steamContextMenu.js
+++ steamContextMenu.js
@@ -1,21 +1,64 @@
-/*
- * Search Steam Context Menu
- */
-chrome.contextMenus.create({
-    "title": "Search Steam for '%s'",
-    "contexts": ["selection"],
-    "onclick": function (info) {
-        chrome.tabs.create({url: 'http://store.steampowered.com/search/?term=' + encodeURIComponent(info.selectionText)});
-    }
-});
+var options = {
+      b_steam: true,
+      b_steamdb: true,
+      b_options: true
+    };
 
-/*
- * Search Steam DB Context Menu
- */
-chrome.contextMenus.create({
+chrome.storage.sync.get(options, update_menus);
+chrome.storage.onChanged.addListener(options_changed);
+
+function options_changed(changes, areaName) {
+  for(var opt in changes) {
+    options[opt] = changes[opt].newValue;
+  }
+  update_menus(options);
+}
+
+function update_menus(results) {
+  options = results;
+  remove_all_menus(function() {
+    if (options.b_steam) {
+      create_steam_menu();
+    }
+    if (options.b_steamdb) {
+      create_steamdb_menu();
+    }
+    if (options.b_options) {
+      create_options_menu();
+    }
+  });
+}
+
+function create_steam_menu() {
+  chrome.contextMenus.create({
+      "title": "Search Steam for '%s'",
+      "contexts": ["selection"],
+      "onclick": function (info) {
+          chrome.tabs.create({url: 'http://store.steampowered.com/search/?term=' + encodeURIComponent(info.selectionText)});
+      }
+  });
+}
+
+function create_steamdb_menu() {
+  chrome.contextMenus.create({
     "title": "Search Steam DB for '%s'",
     "contexts": ["selection"],
     "onclick": function (info) {
         chrome.tabs.create({url: 'https://steamdb.info/search/?q=' + encodeURIComponent(info.selectionText)});
     }
-});
+  });
+}
+
+function create_options_menu() {
+  chrome.contextMenus.create({
+    "title": "Change Visible Menus...",
+    "contexts": ["selection"],
+    "onclick": function (info) {
+        chrome.runtime.openOptionsPage();
+    }
+  });
+}
+
+function remove_all_menus(callback) {
+  chrome.contextMenus.removeAll(callback);
+}
