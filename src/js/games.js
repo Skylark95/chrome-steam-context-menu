ac8270f351f90118ee27a2b76a529ec18b0ef484
Derek <derek@skylark95.com>
2016-09-25 18:22:32 -0500

    implement background page call to steam with caching

--- src/js/games.js
+++ src/js/games.js
@@ -1,15 +1,25 @@
 var params = window.location.search ? new URLSearchParams(window.location.search.slice(1)) : new URLSearchParams('');
 
-chrome.runtime.sendMessage({user: 'test'}, function(response) {
-  console.log(response.data);
-});
-
-$.get('/api/games.json', '', buildTable, 'json');
-
 $('#user-form').on('submit', function(e) {
   e.preventDefault();
   loadGamesForUser(this.user.value);
 });
+
+loadPage();
+
+function loadPage() {
+  var user = params.get('u'),
+      reload = params.get('r') === '1';
+  if (user) {
+    $("#user-form input[name='user']").val(user);
+    chrome.runtime.sendMessage({user: user, reload: reload}, function(response) {
+      buildTable(response.data);
+    });
+  } else {
+    $('#loading').addClass('hidden');
+    $('#no-username').removeClass('hidden');
+  }
+}
 
 function buildTable(data) {
   var $tbody = $('#games tbody'),
@@ -25,23 +35,18 @@ function buildTable(data) {
         pageLength: 15,
         lengthMenu: [[10, 15, 25, 50, 100, -1], [10, 15, 25, 50, 100, "All"]]
       };
-
-  if (user) {
-    $("#user-form input[name='user']").val(user);
-
-    if (query) {
-      options.search = {
-        search: query
-      };
-    }
-    $tbody.parent().removeClass('hidden');
-    $tbody.parent().DataTable(options);
-  } else {
-    $('#no-username').removeClass('hidden');
+  if (query) {
+    options.search = {
+      search: query
+    };
   }
+  $('#loading').addClass('hidden');
+  $tbody.parent().removeClass('hidden');
+  $tbody.parent().DataTable(options);
 }
 
 function loadGamesForUser(user) {
   params.set('u', user);
+  params.set('r', '1');
   window.location.assign(window.location.pathname + '?' + params.toString());
 }
--- src/js/gamesBackgroud.js
+++ src/js/gamesBackgroud.js
@@ -1,16 +1,29 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.user) {
-      var url = 'chrome-extension://' + chrome.runtime.id + '/api/games.xml';
-      get(url, function(xml) {
-          sendResponse({
-            data: buildAndCacheResponse(xml)
+      chrome.storage.local.get({
+        user: '',
+        cacheTime: 0
+      }, function(items) {
+        if (items.user === request.user && !request.reload && (Date.now() - items.cacheTime) < 1800000) {
+          chrome.storage.local.get('gameCache', function (cache) {
+            sendResponse({
+              data: cache.gameCache
+            });
           });
+        } else {
+          var url = 'http://steamcommunity.com/id/' + request.user + '/games/?xml=1';
+          get(url, function(xml) {
+            sendResponse({
+              data: buildAndCacheResponse(request.user, xml)
+            });
+          });
+        }
       });
       return true;
     }
 });
 
-function buildAndCacheResponse(xml) {
+function buildAndCacheResponse(user, xml) {
   var parser = new DOMParser(),
       xmlDoc = parser.parseFromString(xml, 'text/xml'),
       games = xmlDoc.querySelectorAll('gamesList games game'),
@@ -21,6 +34,11 @@ function buildAndCacheResponse(xml) {
       name: buildName(games.item(i))
     });
   }
+  chrome.storage.local.set({
+    user: user,
+    cacheTime: Date.now(),
+    gameCache: out
+  });
   return out;
 }
 
